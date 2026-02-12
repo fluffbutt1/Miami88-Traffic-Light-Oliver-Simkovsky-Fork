987296f6d20e0d785b65fec79a1f5163c62ba70a
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-12 01:19:53 +0000

    started setting up states for traffic light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="d-flex justify-content-center">
-			<div className="bg-black w-25 text-center d-flex flex-column justify-content-center align-items-center">
-				<div className="text-danger circle1"></div>
-				<div className="text-warning circle2"></div>
-				<div className="text-success circle3"></div>
-			</div>
-		</div>
-	);
+
+  const [isActive, setIsActive] = useState(false);
+  
+  const Colors = () =>{
+	setIsActive(true);
+	if(isActive === true){
+		console.log("triggered");
+	}
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="bg-black w-25 text-center d-flex flex-column justify-content-center align-items-center">
+        <div className="circle1" onClick={(Colors)}></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+
+		<div className={`circle4 ${isActive ? "active" : ""}`} onClick={(Colors)}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
