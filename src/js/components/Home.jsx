00decc57a7b186c2bd72a1cd7d4d880948123395
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-12 00:27:08 +0000

    set up structure for traffic light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,8 +6,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			hello
+		<div className="d-flex justify-content-center">
+			<div className="bg-black w-75 text-center">
+				<div className="text-danger">hi</div>
+				<div className="text-warning">hi</div>
+				<div className="text-success">hi</div>
+			</div>
 		</div>
 	);
 };
