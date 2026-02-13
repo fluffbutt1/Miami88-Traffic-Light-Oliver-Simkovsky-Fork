bdcfa274d92d0b0d1c1bda7e4be272f8b877cabc
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-13 23:59:43 +0000

    refactored code to use an array instead of booleans for setting the react state

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,25 +6,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-  const [isActive, setIsActive] = useState(false); 
+  //const [isActive, setIsActive] = useState(false); 
   // for future reference don't use a bool. check for the color. not  if it's true.
   // maybe try useEffect
-  
-  const Colors = () =>{
-	setIsActive(true);
-	if(isActive === true){
-		console.log("triggered");
-		setIsActive(prev => !prev);
-	}
+
+  const [activeColor, setActiveColor] = useState(null);
+
+  const Colors = (color) =>{
+    setActiveColor(color);
+    console.log("cicked");
   };
 
   return (
     <div className="d-flex justify-content-center">
       <div className="bg-black w-25 text-center d-flex flex-column justify-content-center align-items-center">
-        <div className={isActive ? "circle1" : "circle4"} onClick={(Colors)}></div>
-        <div className={isActive ? "circle2" : "circle4"} onClick={(Colors)}></div>
-        <div className={isActive ? "circle3" : "circle4"} onClick={(Colors)}></div>
-		<div className={`circle4 ${isActive ? "active" : ""}`} onClick={(Colors)}></div>
+        <div className={`circle1 ${activeColor === "Green" ? "active" : ""}`} onClick={() => Colors("Green")}></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+		    <div className="circle4"></div>
       </div>
     </div>
   );
